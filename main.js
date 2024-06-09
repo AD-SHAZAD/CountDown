619a5e3cb6363b39f2ef7fdb48c4b16d6b0dcdaa
AD-SHAZAD <167025579+AD-SHAZAD@users.noreply.github.com>
2024-06-09 14:04:12 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,6 +2,12 @@ const START_ = 1717083934104 // variable storing page created time
 const DOB_ =  1704067200000
 var _c_time = START_
 var _displayA = '30/6/2024'
+Map = {
+    getUnit : Change_Units,
+    getWhole : Break
+}
+
+var _MODE_ = 'N'
 
 
 function TimeSince( old_time ){ // Function to Get time Since the given date .
@@ -24,7 +30,12 @@ function Change_Units( input ){ // Take milisecond ( unix time ) value and chang
         DAYS:     Days,
         HOURS:    Hours,
         MINIUTES: Minutes,
-        SECONDS:  Seconds
+        SECONDS:  Seconds,
+        Y:        Years,
+        D:        Days,
+        H:        Hours,
+        M:        Minutes,
+        S:        Seconds
     }
 
 }
@@ -46,14 +57,6 @@ function Break( input ){
     }
 }
 
-
-//---------------------------------------------------------------------------------
-
-
-
-
-
-
 document.getElementById('Save_List').addEventListener('click', () => {
     console.log(' save button clicked')
     let Di = Check_( [ 1 , 31 ] , 'input_date')
@@ -64,6 +67,35 @@ document.getElementById('Save_List').addEventListener('click', () => {
 
 
 })
+
+function Select_Mode( _mode ){
+    _MODE_ = _mode;
+    document.querySelectorAll('._modes').forEach( Element => {
+        Element.style.color = 'white'
+    })
+    document.getElementById(`_mode${_mode}`).style.color = 'cyan'
+}
+
+document.getElementById('_modeN').addEventListener('click', () => {
+    Select_Mode( 'N' )
+})
+
+document.getElementById('_modeS').addEventListener('click', () => {
+    Select_Mode( 'S' )
+})
+
+document.getElementById('_modeM').addEventListener('click', () => {
+    Select_Mode( 'M' )
+})
+
+document.getElementById('_modeH').addEventListener('click', () => {
+    Select_Mode( 'H' )
+})
+
+document.getElementById('_modeD').addEventListener('click', () => {
+    Select_Mode( 'D' )
+})
+
 
 function Check_( range_ , input_id ){
     let input_number = document.getElementById(input_id).value
@@ -91,10 +123,6 @@ function unix_converter( D , M , Y ){
     output_ = date.getTime()
     return output_
 }
-
-
-
-
 // mainLoop Function ! to be called every 1000ms for time updation >->
 function MainLoop() {
     
@@ -103,16 +131,28 @@ function MainLoop() {
     if( _c_time > Date.now() ){
 
         X = Break( TimeUntill( _c_time ).SECONDS)
-        document.getElementById('display').innerHTML = `${X.D}:${X.H}:${X.M}:${X.S}`
+
+        if( _MODE_ == 'N' ){
+            document.getElementById('display').innerHTML = `${X.D}:${X.H}:${X.M}:${X.S}`
+        } else {
+            document.getElementById('display').innerHTML = `${ TimeUntill( _c_time )[_MODE_]}`
+        }
+
+        // document.getElementById('display').innerHTML = `${X.D}:${X.H}:${X.M}:${X.S}`
         document.getElementById('display_a').innerHTML = `Till  ${_displayA}`
         document.getElementById('display_p').innerHTML = `${getPercentageOfYear()}   2024`
         // console.log('Untill')
-
+        
     } else {
 
         X = Break( TimeSince( _c_time).SECONDS)
 
-        document.getElementById('display').innerHTML = `${X.D}:${X.H}:${X.M}:${X.S}`
+        // document.getElementById('display').innerHTML = `${X.D}:${X.H}:${X.M}:${X.S}`
+        if( _MODE_ == 'N' ){
+            document.getElementById('display').innerHTML = `${X.D}:${X.H}:${X.M}:${X.S}`
+        } else {
+            document.getElementById('display').innerHTML = `${ TimeSince( _c_time )[_MODE_]}${_MODE_.toLowerCase()}`
+        }
         document.getElementById('display_a').innerHTML = `Since ${_displayA}`
         document.getElementById('display_p').innerHTML = `${getPercentageOfYear()}  /  2024`
 
